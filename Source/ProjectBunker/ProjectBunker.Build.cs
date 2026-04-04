566310d74989b23ec63cde265c404b8be820bc9f
K0brin <nlundy2099@gmail.com>
2026-04-04 15:26:53 -0500

    initial commit

---
+++ Source/ProjectBunker/ProjectBunker.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ProjectBunker : ModuleRules
+{
+	public ProjectBunker(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"ProjectBunker",
+			"ProjectBunker/Variant_Horror",
+			"ProjectBunker/Variant_Horror/UI",
+			"ProjectBunker/Variant_Shooter",
+			"ProjectBunker/Variant_Shooter/AI",
+			"ProjectBunker/Variant_Shooter/UI",
+			"ProjectBunker/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/ProjectBunker/ProjectBunkerCameraManager.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "ProjectBunkerCameraManager.h"
+
+AProjectBunkerCameraManager::AProjectBunkerCameraManager()
+{
+	// set the min/max pitch
+	ViewPitchMin = -70.0f;
+	ViewPitchMax = 80.0f;
+}
